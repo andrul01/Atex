8bef983f0e881a31201a8a8f7935384dd8a58690
Andrul01 <andrumorallies@gmail.com>
2024-11-12 17:03:49 +0530

    added atex.sql file

--- db/atex.sql
+++ db/atex.sql
@@ -1,3 +1,22 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2024 at 12:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `atex`
 --
@@ -12,8 +31,8 @@ CREATE TABLE `categories` (
   `id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `categories`
@@ -40,28 +59,30 @@ CREATE TABLE `comments` (
   `comments_id` int(8) NOT NULL,
   `comment_content` text NOT NULL,
   `comment_by` int(8) NOT NULL,
-  `comment_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment_time` datetime NOT NULL DEFAULT current_timestamp(),
   `thread_id` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `comments`
 --
 
 INSERT INTO `comments` (`comments_id`, `comment_content`, `comment_by`, `comment_time`, `thread_id`) VALUES
-(1, 'use apt update && apt upgrade \r\nthen pkg install python', 1, '2024-09-02 17:22:42', 1),
-(2, 'use apt update && apt upgrade \r\nthen pkg install python', 3, '2024-09-02 17:23:22', 1),
-(3, 'sdfjsdkhfjdsf', 4, '2024-09-02 17:25:05', 1),
-(4, 'adsopwepyjhbdkjasd', 4, '2024-09-02 17:25:10', 1),
-(5, 'fdsiwehewqbwkjfbds', 1, '2024-09-02 17:25:15', 1),
-(6, 'fewrfj;fqhwf\r\n', 2, '2024-09-02 17:25:21', 1),
-(7, 'adsopwepyjhbdkjasd', 3, '2024-09-02 17:25:27', 1),
-(8, 'pkg install python2 ', 1, '2024-09-03 15:53:35', 9),
-(9, 'simple restart the pc ', 7, '2024-09-03 16:32:30', 16),
-(10, 'simple restart the pc ', 7, '2024-09-03 16:47:20', 16),
-(11, '&gt;', 7, '2024-09-04 13:08:02', 1),
-(12, '&lt;script>  \r\n  alert("andrul");\r\n&lt;/script>', 7, '2024-09-04 13:10:55', 1),
-(13, 'use \r\npkg install python \r\nit will work properly', 8, '2024-09-04 15:14:34', 1);
+(14, 'from pa import library', 11, '2024-10-17 13:21:27', 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `sno` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `message` text NOT NULL,
+  `dt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -76,23 +97,16 @@ CREATE TABLE `threads` (
   `thread_description` text NOT NULL,
   `thread_cat_id` int(7) NOT NULL,
   `thread_user_id` int(7) NOT NULL,
-  `dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `dt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `threads`
 --
 
 INSERT INTO `threads` (`sno`, `thread_id`, `thread_title`, `thread_description`, `thread_cat_id`, `thread_user_id`, `dt`) VALUES
-(0, 1, 'python installation ', 'in termux I am unable to install python ', 1, 1, '2024-09-02 17:20:54'),
-(0, 2, 'error', 'ffdsfdsfoiuo;rpwe', 1, 4, '2024-09-02 17:25:50'),
-(0, 3, 'error', 'ffdsfdsfoiuo;rpwe', 1, 3, '2024-09-02 17:26:49'),
-(0, 13, 'asdgf', 'asgsfdg', 1, 0, '2024-09-03 16:10:22'),
-(0, 14, 'er6ty647y', '4556345232224ffg', 1, 1, '2024-09-03 16:13:02'),
-(0, 15, 'er6ty647y', '4556345232224ffg', 1, 1, '2024-09-03 16:25:21'),
-(0, 16, 'Reload error ', 'while reload python got error', 1, 7, '2024-09-03 16:31:48'),
-(0, 17, 'java setup', 'unable to setup', 2, 7, '2024-09-03 16:53:40'),
-(0, 28, 'hello', '&lt;script> alert("andrul"); &lt;/script>', 1, 7, '2024-09-04 13:22:23');
+(0, 29, 'python installation', 'unable to install python in vs code ', 1, 11, '2024-10-08 14:13:22'),
+(0, 30, 'script not working', 'import pandas not working ', 1, 12, '2024-10-17 13:20:05');
 
 -- --------------------------------------------------------
 
@@ -104,17 +118,16 @@ CREATE TABLE `users` (
   `sno` int(8) NOT NULL,
   `user_email` varchar(30) NOT NULL,
   `user_pass` varchar(255) NOT NULL,
-  `dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `dt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`sno`, `user_email`, `user_pass`, `dt`) VALUES
-(7, 'andrul', '$2y$10$TJGqifRUlr.sOaCCeuP1iedqiF1st7J8vMnB/B/7PN1Nco03Rx6oa', '2024-09-03 16:28:35'),
-(8, 'vedant', '$2y$10$TJ3c3x.9.KaY58KQ/Fzze.hG/AN491M1Ro6foBdZz02KWORRsCqzm', '2024-09-04 15:13:32'),
-(9, 'mit', '$2y$10$igXEoTpUM3V4UBjGxRv//O/I1PRzNNE/AL5gC1sbvNoqUUYrtWMz.', '2024-09-04 17:50:44');
+(11, 'andrul', '$2y$10$874mXguVSYvLu2aIMXCNK.OzxSixJZcZEnaYkUODTHO5d4zkP8qma', '2024-10-08 14:12:20'),
+(12, 'dev', '$2y$10$aOCZyT0omN3V2kIcVDz4lO4e9MfbU9k/ZDUYiaKDf/WQicgzEE8Sy', '2024-10-17 13:17:53');
 
 --
 -- Indexes for dumped tables
@@ -131,6 +144,12 @@ ALTER TABLE `categories`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`comments_id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`sno`);
 
 --
 -- Indexes for table `threads`
@@ -153,21 +172,32 @@ ALTER TABLE `users`
 --
 ALTER TABLE `categories`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comments_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `comments_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `sno` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `threads`
 --
 ALTER TABLE `threads`
-  MODIFY `thread_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `thread_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `sno` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `sno` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
