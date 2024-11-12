da94feb7f2eabf9c5f299153503388291441ab98
Andrul01 <andrumorallies@gmail.com>
2024-11-13 02:39:45 +0530

    little change in footer and index file

--- Partials/_footer.php
+++ Partials/_footer.php
@@ -2,7 +2,7 @@
 
 echo '
 <div class="container-fluid  text-light bg-dark  mb-0">
-  <p class="text-center py-2 mb-0" >Copyright Atex tech forum 2024 || All rights reserved</p>
+  <p class="text-center py-2 mb-0" >Copyright <span class="text-primary">Atex</span> tech forum 2024 || All rights reserved</p>
 </div>';
 
 ?>
